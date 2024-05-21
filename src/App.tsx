cce9f0c2dd06f16b31f8f00e52ac8ea4de4b3895
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-21 17:15:10 +0200

    Create a new tag Button with component onClickHandletMinus

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import './App.css';
 function App() {
 
     let [alpha, setAlpha] = useState(1)
-    const onClickHandler = () => {
+
+    const onClickHandlerPlus = () => {
         setAlpha(++alpha)
         console.log(alpha)
     }
+
+    const onClickHandlerMinus = () => {
+        setAlpha(--alpha)
+        console.log(alpha)
+    }
+
     return (
         <div className="App">
             <h1>{alpha}</h1>
-            <button onClick={onClickHandler}>NUMBER +</button>
+            <button onClick={onClickHandlerPlus}>NUMBER +</button>
+            <button onClick={onClickHandlerMinus}>NUMBER -</button>
         </div>
     );
 }
